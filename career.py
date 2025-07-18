4c12b7716dafaf97af1d5f14c4f7940d0a14640d
Sanchithedau <sanchit.hedau24@mmit.edu.in>
2025-07-18 09:03:48 +0530

    career.py

--- career.py
+++ career.py
@@ -2,19 +2,19 @@ from experta import *
 class StudentFacts(Fact):
     pass
 class CareerExpertSystem(KnowledgeEngine):
-    @Rule(StudentFacts(likes='Maths'), StudentFacts(likes='Physics'))
-    def mechanical(self):
-        print("Suggested Career Path: Mechanical Engineering")
     @Rule(StudentFacts(likes='Programming'), StudentFacts(likes='Maths'))
     def computer(self):
         print("Suggested Career Path: Computer Engineering")
-    @Rule(StudentFacts(likes='Biology'), StudentFacts(likes='Chemistry'))
-    def biotech(self):
-        print("Suggested Career Path: Biotechnology")
     @Rule(StudentFacts(likes='Circuits'), StudentFacts(likes='Maths'))
     def electronics(self):
         print("Suggested Career Path: Electronics Engineering")
-
+    @Rule(StudentFacts(likes='Maths'), StudentFacts(likes='Physics'))
+    def mechanical(self):
+        print("Suggested Career Path: Mechanical Engineering")
+    @Rule(StudentFacts(likes='Biology'), StudentFacts(likes='Chemistry'))
+    def biotech(self):
+        print("Suggested Career Path: Biotechnology")
+        
 def main():
     engine = CareerExpertSystem()
     engine.reset()
